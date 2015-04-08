a1d75ea7cd28ca05ae511b7a7d6d8473fee7c827
eiamnacken <sven2q@hotmail.de>
2015-04-08 23:27:43 +0200

    Aufgabe 4 Funktionell fertig gestellt fehlt nur noch die Dokumentation wird auf Freitag abend verschoben stay tuned

--- Aufgabe4/ceasar.c
+++ Aufgabe4/ceasar.c
@@ -3,24 +3,29 @@
  *  Autor:Sven Marquardt
  *  Created on:08.04.2015
 */
-
+#include <stdio.h>
 #include <ceasar.h>
 
 void ceasar(char text[], char offset){
-    int i=0;
+    unsigned int i=0;
+    int j=0;
     for(i=0;;i++){
         if(text[i]!='\0'){
-            if((text[i]>65&&text[i]<90)||(text[i]>97&&text[i]<122)){
-                if(text[i]==90){
-                    text[i]=65;
-                    text[i]+=(offset-1);
-                }else if(text[i]==122){
-                    text[i]=97;
-                    text[i]+=(offset-1);
-                }else{
-                    text[i]+=offset;
-                }
+            if((text[i]>64&&text[i]<91)||(text[i]>96&&text[i]<123)){
+                for(j=0;j<offset;j++){
+                    if(text[i]==90){
+                        text[i]=65;
+                        text[i]-=1;
 
+
+                    }if(text[i]==122){
+                        text[i]=97;
+                        text[i]-=1;
+
+
+                    }
+                        text[i]+=1;
+                }
             }
         }else{
             break;
--- Aufgabe4/main.c
+++ Aufgabe4/main.c
@@ -10,9 +10,10 @@ int main(int argc, char *argv[])
     int offset=0;
     char decision;
     size_t size = sizeof(string)/sizeof(char);
-    printf("Bitte geben sie an welches Programm sie starten m\u00f6chten\n");
-    printf("1 f\u00fcr Verschl\u00fcsseln oder 2 f\u00fcr Entschl\u00fcsseln 0 zum abbrechen: ");
-    while((decision=getchar())!=0){
+    do{
+        printf("\nBitte geben sie an welches Programm sie starten m\u00f6chten\n");
+        printf("1 f\u00fcr Verschl\u00fcsseln oder 2 f\u00fcr Entschl\u00fcsseln 3 zum abbrechen: ");
+        decision=getchar();
         switch(decision){
         case '1':{
             printf("\n");
@@ -25,37 +26,36 @@ int main(int argc, char *argv[])
             sscanf(buffer,"%d",&offset);
             ceasar(string,offset);
             printf("%s \n",string);
-            printf("Bitte geben sie an welches Programm sie starten m\u00f6chten\n");
-            printf("1 f\u00fcr Verschl\u00fcsseln oder 2 f\u00fcr Entschl\u00fcsseln 0 zum abbrechen: ");
         }
             break;
         case '2':{
             printf("Bitte geben sie ihren zu entschl\u00fcsselnden Satz ein: \n");
             fgets(string,100,stdin);
             fgets(string,100,stdin);
-            changeNewLineIEnd(string,size);
-            rasaec(string);
+            char longText[]="Xl ptk xbg lmktaexgw-dtemxk Tikbemtz, ngw wbx Nakxg lvaenzxgwkxbsxag.Pbglmhg Lfbma, wtl Dbgg tg wbx Uknlm zxshzxg, nf wxf lvaxnllebvaxg Pbgw sn xgmzxaxg, lvaenxiymx ktlva wnkva wbx Zetlmnxkxg wxk Obvmhkr Fbxmldtlxkgx, whva gbvam ktlva zxgnz, nf sn oxkabgwxkg, wtll fbm baf tnva xbg zkbxllbzxk Lmtnupbkuxe axkxbgpxamx.";
+            changeNewLineIEnd(longText,sizeof(longText)/sizeof(char));
+            rasaec(longText);
             printf("Der enschl\u00fcsselte Text:\n");
-            printf("%s",string);
-            printf("Bitte geben sie an welches Programm sie starten m\u00f6chten\n");
-            printf("1 f\u00fcr Verschl\u00fcsseln oder 2 f\u00fcr Entschl\u00fcsseln 0 zum abbrechen: ");
+            printf("%s",longText);
         }
             break;
 
-        case '0':
+        case '3':
             printf("Programm beendet!\n");
+            decision='0';
             break;
         default:
             printf("Falsche eingabe\n");
+            decision='0';
             break;
         }
-    }
+    }while(decision!='0');
 
 
 }
 
 void changeNewLineIEnd(char string[], size_t size){
-    int i=0;
+    unsigned int i=0;
     for(i=0;i<size;i++){
         if(string[i]=='\n'){
             string[i]='\0';
